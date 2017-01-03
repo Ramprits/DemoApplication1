a38daf61b434942d2e93a25c6a617d17246c55d9
Ramprit Sahani <Rampritsahani@outlook.in>
2017-01-03 16:23:30 +0530

    :22

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
-using DemoApplication1.Entities;
 using DemoApplication1.Models;
 using DemoApplication1.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -40,11 +38,45 @@ namespace DemoApplication1.Controllers
 
           }
           [HttpGet("{Id}")]
-          public JsonResult GetCity(int Id)
+          public IActionResult GetCity(int Id, bool IncludePointsOfInterest = false)
           {
-               return new JsonResult(
-                   CitiesDataStore.Current.Citites.FirstOrDefault(pc => pc.Id == Id)
-               );
+               //    return new JsonResult(
+               //        CitiesDataStore.Current.Citites.FirstOrDefault(pc => pc.Id == Id)
+               //    );
+
+               var city = _cityInfoRepository.GetCity(Id, IncludePointsOfInterest);
+               if (city == null)
+               {
+                    return NotFound();
+               }
+
+               if (IncludePointsOfInterest)
+               {
+                    var cityResult = new CityDto()
+                    {
+                         Id = city.Id,
+                         Name = city.Name,
+                         Description = city.Description
+                    };
+                    foreach (var poi in city.PointsOfInterests)
+                    {
+                         cityResult.PointsOfInterest.Add(new PointsOfInterestDto()
+                         {
+                              Id = poi.Id,
+                              Name = poi.Name,
+                              Description = poi.Description
+                         });
+                    }
+                    return Ok(cityResult);
+               }
+
+               var CWpointsOfInterest = new CityWithOutPointsOfInterest()
+               {
+                    Id = city.Id,
+                    Name = city.Name,
+                    Description = city.Description
+               };
+               return Ok(CWpointsOfInterest);
           }
      }
 }
